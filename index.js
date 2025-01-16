3c455df4ba26a065cd1770a0383f3a17d4973205
Jah'swill Emmanuel <jahswill4jahs@gmail.com>
2025-01-16 20:27:16 +0100

    Enhance image carousel with improved styling, keyboard navigation, and dynamic image loading

--- index.js
+++ index.js
@@ -1,118 +1,43 @@
-import { computePosition } from "https://cdn.skypack.dev/@floating-ui/dom";
-
 let previousBtn = document.getElementById("previous");
 let nextBtn = document.getElementById("next");
 let imageContainer = document.getElementById("display-div");
 
-let picOne = document.createElement("img");
-let picTwo = document.createElement("img");
-let picThree = document.createElement("img");
-let picFour = document.createElement("img");
-let picFive = document.createElement("img");
-let picSix = document.createElement("img");
-let picSeven = document.createElement("img");
-
-picOne.src =
-  "https://i.abcnewsfe.com/a/f43853f3-9eaf-4048-9ae7-757332c5787e/mclaren-1-ht-gmh-240412_1712928561648_hpMain_16x9.jpg?w=1600";
-picOne.alt = "First Image";
-
-picTwo.src =
-  "https://static.vecteezy.com/system/resources/thumbnails/023/192/562/small_2x/sport-car-running-on-the-road-in-future-city-created-with-generative-ai-free-photo.jpg";
-picTwo.alt = "Second Image";
-
-picThree.src = "https://i.ytimg.com/vi/0id5rVsErUw/maxresdefault.jpg";
-picThree.alt = "Third Image";
-
-picFour.src =
-  "https://eu-images.contentstack.com/v3/assets/blt0bbd1b20253587c0/blt4bf1a4090380d5b0/651579e1a95ca40cf4969a68/00-Divo_BUGATTI.jpg?disable=upscale&width=1200&height=630&fit=crop";
-picFour.alt = "Fourth Image";
-
-picFive.src =
-  "https://news.dupontregistry.com/wp-content/uploads/2018/02/8901McLaren-Senna-Victory-Grey-18.jpg";
-picFive.alt = "Fifth Image";
-
-picSix.src =
-  "https://di-uploads-pod12.dealerinspire.com/universitydodgeram/uploads/2018/07/2018-Dodge-Challenger-SRT-Demon-University-Dodge.jpg";
-picSix.alt = "Sixth Image";
-
-picSeven.src =
-  "https://robbreport.com/wp-content/uploads/2016/01/5854-mclaren570scoupe-mantisgreen-0051.jpg";
-picSeven.alt = "Seventh Image";
-
-let picture = [picOne, picTwo, picThree, picFour, picFive, picSix, picSeven];
+let images = [
+  "https://i.abcnewsfe.com/a/f43853f3-9eaf-4048-9ae7-757332c5787e/mclaren-1-ht-gmh-240412_1712928561648_hpMain_16x9.jpg?w=1600",
+  "https://static.vecteezy.com/system/resources/thumbnails/023/192/562/small_2x/sport-car-running-on-the-road-in-future-city-created-with-generative-ai-free-photo.jpg",
+  "https://i.ytimg.com/vi/0id5rVsErUw/maxresdefault.jpg",
+  "https://eu-images.contentstack.com/v3/assets/blt0bbd1b20253587c0/blt4bf1a4090380d5b0/651579e1a95ca40cf4969a68/00-Divo_BUGATTI.jpg?disable=upscale&width=1200&height=630&fit=crop",
+  "https://news.dupontregistry.com/wp-content/uploads/2018/02/8901McLaren-Senna-Victory-Grey-18.jpg",
+  "https://di-uploads-pod12.dealerinspire.com/universitydodgeram/uploads/2018/07/2018-Dodge-Challenger-SRT-Demon-University-Dodge.jpg",
+  "https://robbreport.com/wp-content/uploads/2016/01/5854-mclaren570scoupe-mantisgreen-0051.jpg",
+];
 
 let index = 0;
 
 function updatePicture() {
-  imageContainer.innerHTML = "";
-  imageContainer.appendChild(picture[index]);
+  imageContainer.innerHTML = `<img src="${images[index]}" alt="Car ${index + 1}" />`;
 }
 
 previousBtn.addEventListener("click", () => {
-  index = (index - 1 + picture.length) % picture.length;
+  index = (index - 1 + images.length) % images.length;
   updatePicture();
-//   positionElements(); // Reposition after image change
 });
 
 nextBtn.addEventListener("click", () => {
-  index = (index + 1) % picture.length;
+  index = (index + 1) % images.length;
   updatePicture();
-//   positionElements(); // Reposition after image change
 });
 
-function positionElements() {
-  const screenWidth = window.innerWidth;
-
-  if (screenWidth > 650) {
-    computePosition(imageContainer, previousBtn, {
-      placement: "left",
-    }).then(({ x, y }) => {
-      Object.assign(previousBtn.style, {
-        top: `${y}px`,
-        left: `${x}px`,
-      });
-    });
-
-    computePosition(imageContainer, nextBtn, {
-      placement: "right",
-    }).then(({ x, y }) => {
-      Object.assign(nextBtn.style, {
-        top: `${y}px`,
-        left: `${x}px`,
-      });
-    });
-  } else {
-    computePosition(imageContainer, previousBtn, {
-      placement: "bottom-start",
-    }).then(({ x, y }) => {
-      Object.assign(previousBtn.style, {
-        top: `${y}px`,
-        left: `${x}px`,
-      });
-    });
-
-    computePosition(imageContainer, nextBtn, {
-      placement: "bottom-end",
-    }).then(({ x, y }) => {
-      Object.assign(nextBtn.style, {
-        top: `${y}px`,
-        left: `${x}px`,
-      });
-    });
+// Add keyboard navigation
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    previousBtn.click();
+  } else if (e.key === "ArrowRight") {
+    nextBtn.click();
   }
-}
-
-// Debounced resize event handler
-let resizeTimeout;
-window.addEventListener("resize", () => {
-  clearTimeout(resizeTimeout);
-  resizeTimeout = setTimeout(positionElements, 100);
 });
 
-// Ensure images are loaded before positioning elements
+// Initialize the carousel
 document.addEventListener("DOMContentLoaded", () => {
   updatePicture();
-  picture.forEach((img) => {
-    img.onload = positionElements;
-  });
 });
